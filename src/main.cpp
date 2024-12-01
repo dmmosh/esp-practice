70b391eba23463c8ca6f6ab764410a7534ba564d
dmmosh <evilsharkeater@gmail.com>
2024-12-01 00:19:19 -0500

    switched to arduino but it wokrs now

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LED 2 // led
+
+void led(void* args){
+  int i = 1000;
+  while(1){
+    digitalWrite(LED, HIGH);
+    vTaskDelay(i/portTICK_PERIOD_MS);
+    digitalWrite(LED, LOW);
+    vTaskDelay(i/portTICK_PERIOD_MS);
+    i= (int)((float)i /1.2);
+    if(i == 0){
+      i = 1000;
+    }
+    Serial.printf("%i\n");
+  }
+}
+
+/*
+RUN AND COMPILE
+pio run && pio run --target upload && git-all && pio device monitor
+
+
+*/
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+
+  xTaskCreate(led, "blink led", 2048, NULL, 1, NULL);
+
+}
+
+void loop() {}
