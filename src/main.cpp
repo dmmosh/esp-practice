3fbe14351510ff5c66e70f54be242e0a1803085b
dmmosh <evilsharkeater@gmail.com>
2024-12-01 21:51:47 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <lvgl.h>
 
 #define LED 2 // led
+#define X_RES   100
+#define Y_RES   100
+#define TFT_ROTATION  LV_DISPLAY_ROTATION_0
 
 void led(void* args){
   int i = 1000;
@@ -30,10 +33,24 @@ pio run; git-all; pio run --target upload && pio device monitor
 
 */
 
+static uint32_t my_tick(void)
+{
+    return millis();
+}
+
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(LED, OUTPUT);
+  lv_init();
+  lv_tick_set_cb(my_tick);
+
+  lv_display_t * disp;
+  disp = lv_display_create(X_RES, TFT_VER_RES);
+
+  lv_display_set_flush_cb(disp, my_disp_flush);
+  lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+
 
   xTaskCreate(led, "blink led", 2048, NULL, 1, NULL);
 
