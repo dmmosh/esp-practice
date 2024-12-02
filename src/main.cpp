fcad85fbc64204508f68f6b33fbe8a40c7474477
dmmosh <evilsharkeater@gmail.com>
2024-12-02 01:22:25 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #include <lvgl.h>
 
 #define LED 2 // led
+#define BACKLIGHT 32 //backlight cpio
+#define X_RES 100
+#define Y_RED 100
+
 
 void led(void* args){
   int i = 1000;
@@ -23,64 +27,37 @@ void led(void* args){
   }
 }
 
-// /*
-// RUN AND COMPILE (with git-all)
-// pio run; git-all; pio run --target upload && pio device monitor
-
-
-// */
-
-// static uint32_t my_tick(void)
-// {
-//     return millis();
-// }
-
-// void setup() {
-//   Serial.begin(9600);
-//   // put your setup code here, to run once:
-//   pinMode(LED, OUTPUT);
-//   lv_init();
-//   lv_tick_set_cb(my_tick);
-
-//   lv_obj_t *label = lv_label_create( lv_screen_active() );
-//   lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
-//   lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
-
-
-//   xTaskCreate(led, "blink led", 2048, NULL, 1, NULL);
-
-// }
-
-// void loop() {}
-
-/*Using LVGL with Arduino requires some extra steps:
- *Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html  */
-
-#include <TFT_eSPI.h> // Include TFT_eSPI library
-#define BACKLIGHT 32
 
 TFT_eSPI tft = TFT_eSPI(); // Create TFT object
 
-void setup() {
-  Serial.begin(9600);
-  tft.init();
-  tft.setRotation(1);  // Set display rotation (optional)
+static uint32_t my_tick(void)
+{
+    return millis();
+}
 
-  // Change background to black
-  tft.fillScreen(TFT_WHITE);
+void setup() {
+  Serial.begin(9600); //listen on port 9600
+  tft.init(); // init serial connection
+  analogWrite(BACKLIGHT, 255); // make brightness max
+  pinMode(BACKLIGHT, OUTPUT); // set brightness
+  xTaskCreate(led, "blink led", 2048, NULL, 1, NULL); //blinking led task
+  //tft.setRotation(1);  // Set display rotation (optional)
+  lv_init();
+  lv_tick_set_cb(my_tick);
+
+  lv_display_t* disp = lv_display_create(X_RES, Y_RED);
+  lv_display_set_rotation(disp, LV_DISP_ROTATION_90);
+  lv_obj_t *label = lv_label_create( lv_screen_active() );
+    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+
+    Serial.println( "Setup done" );
   
-  // Print some text
-  tft.setTextColor(TFT_BLACK);  // Set text color to white
-  tft.setTextSize(2);  // Set text size
-  tft.setCursor(10, 10);  // Set cursor position
-  tft.println("Background is Black");
-  pinMode(BACKLIGHT, OUTPUT);
-  analogWrite(BACKLIGHT, 255);
-  
-  xTaskCreate(led, "blink led", 2048, NULL, 1, NULL);
 
 }
 
 void loop() {
+  lv_timer_handler();
+  delay(5);
   // You can add more functionality here
 }
