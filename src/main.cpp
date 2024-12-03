668a5f65c79de2d95cc581609af6d141c6982b84
dmmosh <evilsharkeater@gmail.com>
2024-12-02 22:26:29 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ void setup() {
   lv_obj_t * label = lv_label_create(CURR);
   lv_label_set_text(label, "HELLO WORLD");
   //lv_obj_set_style_text_color(label,lv_color_black(), 0);
-  lv_obj_align(label, LV_ALIGN_TOP_LEFT, 10,40);
+  lv_obj_align(label, LV_ALIGN_CENTER, 0,0);
 
 
   //tft.setCursor(x_set, y_set);  // Set cursor position
