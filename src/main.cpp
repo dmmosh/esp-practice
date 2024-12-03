481aeef83b3f0d9a620a23db44f2fbc17ac9f67f
dmmosh <evilsharkeater@gmail.com>
2024-12-02 22:25:17 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ void setup() {
 
   lv_obj_t * label = lv_label_create(CURR);
   lv_label_set_text(label, "HELLO WORLD");
-  lv_obj_set_style_text_color(label,lv_color_black(), LV_PART_MAIN|LV_STATE_DEFAULT);
+  //lv_obj_set_style_text_color(label,lv_color_black(), 0);
   lv_obj_align(label, LV_ALIGN_TOP_LEFT, 10,40);
 
 
