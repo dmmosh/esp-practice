285af40c73702459bd1d0a5a3606a97c590ee665
dmmosh <evilsharkeater@gmail.com>
2024-12-02 23:38:35 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 #define LED 2 // led
 #define BACKLIGHT 32 //backlight cpio
-#define X_RES 200 // y (rotated)
+#define X_RES 170 // y (rotated)
 #define Y_RES 320 // x (rotated)
 #define DRAW_BUF_SIZE (X_RES * Y_RES / 10 * (LV_COLOR_DEPTH / 8))
 TFT_eSPI tft = TFT_eSPI(); // Create TFT object
