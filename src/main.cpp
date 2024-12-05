79b402a60d449f7e3970da5c407e814a123152eb
dmmosh <evilsharkeater@gmail.com>
2024-12-05 17:39:16 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -120,8 +120,6 @@ void setup() {
 
   uint16_t seconds = 0;
   while(WiFi.status() != WL_CONNECTED){
-    debug(ssid);
-    debug(pass);
     debug("connecting %is", (unsigned int)seconds);
     delay(1000);
     seconds++;
@@ -136,9 +134,54 @@ void setup() {
 
 
 void loop() {
-
+  WiFiClient client = server.available();
 
   delay(5);
+  if (!client) return;
+  String curr = "";
+  while (client.connected()) {    // loop while the client's connected
+      if (client.available()) {     // if there's bytes to read from the client,
+        char c = client.read();     // read a byte, then
+        Serial.write(c);            // print it out the serial monitor
+        if (c == '\n') {            // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (curr.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {  // if you got a newline, then clear curr:
+            curr = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          curr += c;      // add it to the end of the curr
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (curr.endsWith("GET /H")) {
+          digitalWrite(5, HIGH);  // GET /H turns the LED on
+        }
+        if (curr.endsWith("GET /L")) {
+          digitalWrite(5, LOW);  // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+
   //lv_timer_handler();
 }
 
