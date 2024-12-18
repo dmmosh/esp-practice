71091708ed0966c4aaa604c03149a22a9bfe0719
wettestsock <evilsharkeater@gmail.com>
2024-12-17 21:10:42 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <Arduino.h>
 #include "freertos/FreeRTOS.h"
+#define LED 23
 
+
+void blink(void* args){
+    while(1){
+        digitalWrite(LED, HIGH);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        digitalWrite(LED, LOW);
+        Serial.print("LED BLINKED. TESTING.\n");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+
+    }
+
+}
 
 
 void setup() {
     Serial.begin(115200);
-    
+    pinMode(LED, OUTPUT);
+
+    xTaskCreate(blink, "led blink",1028,NULL,1,NULL);
   // put your setup code here, to run once:
 }
 
@@ -15,7 +30,8 @@ pio device monitor -b 115200
 */
 
 void loop() {
-    Serial.print("fdksjljd");
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+
+
+    vTaskDelay(5/portTICK_PERIOD_MS);
   // put your main code here, to run repeatedly:
 }
