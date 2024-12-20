534b2dfcbc457693e13ed28c892188d241b307ee
wettestsock <evilsharkeater@gmail.com>
2024-12-19 20:24:08 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,12 @@ WiFiClient server(80);
 String input;
 
 
-String input_string(const bool pass){
+String input_string(){
     String out;
     while(1){
         if(Serial.available() >0){
             char curr = Serial.read();
             //str.trim();
-
-           
             switch(curr){
                 case '\n':
                     if(out.length()>1){
@@ -32,8 +30,6 @@ String input_string(const bool pass){
                 break;
                 default:
                     out += curr;
-                    if(pass) curr = '*';
-                    
                     Serial.print(curr);
                 break;
             }
@@ -41,59 +37,38 @@ String input_string(const bool pass){
     }
 }
 
+
 void setup() {
-  Serial.begin(115200);
-  Serial.setTimeout(10);
+    Serial.begin(115200);
+    Serial.setTimeout(10);
+    Serial.println("Wifi name: ");
+    String name = input_string();
 
-  
+    Serial.println("Wifi password: ");
+    String pass = input_string();
+
+    WiFi.begin(name,pass);
+    uint16_t seconds = 0;
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.printf("Connecting... %is\n", seconds);
+        seconds++;
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+    Serial.println("WiFi connected %is\n");
+    Serial.println("ip: ");
+    Serial.println(WiFi.localIP());
 }
+
 void loop() {
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.println("WiFi disconnected.\n");
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        return;
+    }
 
-  String input1 = input_string(true);
-  Serial.println(input1);
-  String input2 = input_string(false);
-  Serial.println(input2);
 
 
+    Serial.print("hi ");
+
+    vTaskDelay(1000/portTICK_PERIOD_MS);
 }
-
-// void setup() {
-//     Serial.begin(115200);
-//     Serial.setTimeout(10);
-//     Serial.println("Wifi name: ");
-//     String name = Serial.readString();
-//     while(*name.end() != '\n'){
-//         vTaskDelay(5/portTICK_PERIOD_MS);
-//     }   
-
-//     Serial.println("Wifi password: ");
-//     String pass = Serial.readString();
-//     while(*pass.end() != '\n'){
-//         vTaskDelay(5/portTICK_PERIOD_MS);
-//     }   
-
-//     WiFi.begin(name,pass);
-//     uint16_t seconds = 0;
-//     while(WiFi.status() != WL_CONNECTED){
-//         Serial.printf("Connecting... %is\n", seconds);
-//         seconds++;
-//         vTaskDelay(1000/portTICK_PERIOD_MS);
-//     }
-//     Serial.println("WiFi connected %is\n");
-//     Serial.println("ip: ");
-//     Serial.println(WiFi.localIP());
-// }
-
-// void loop() {
-//     if(WiFi.status() != WL_CONNECTED){
-//         Serial.println("WiFi disconnected.\n");
-//         vTaskDelay(1000/portTICK_PERIOD_MS);
-//         return;
-//     }
-
-
-
-//     Serial.print("hi ");
-
-//     vTaskDelay(1000/portTICK_PERIOD_MS);
-// }
