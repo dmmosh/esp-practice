98c14743fb6c7e4ca062a329550f939ddde8d354
dmmosh <evilsharkeater@gmail.com>
2025-01-23 20:22:47 -0500

    nothing of note

---
+++ main/esp-practice.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+
+void app_main(void)
+{
+    gpio_reset_pin(GPIO_NUM_2);
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+
+    while(1){
+        printf("On!\n");
+        gpio_set_level(GPIO_NUM_2, 1);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        printf("Off!\n");
+        gpio_set_level(GPIO_NUM_2, 0);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+}
