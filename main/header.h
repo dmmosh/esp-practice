289a6b466db6a891f2fc997f7a5ee70c43cc4159
dmmosh <evilsharkeater@gmail.com>
2025-01-23 21:07:23 -0500

    nothing of note

--- main/header.h
+++ main/header.h
@@ -1,13 +1,15 @@
-#include <stdio.h>
+#include "esp_log.h"
+#include "esp_hidd_api.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_bt.h"
+#include "esp_err.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_gap_bt_api.h"
+#include <string.h>
 #include <inttypes.h>
-#include "sdkconfig.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_flash.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "driver/gpio.h"
-
-
-
-void blink();
+#include "freertos/semphr.h"
--- main/main.cpp
+++ main/main.cpp
@@ -4,21 +4,7 @@
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
 
-#include "esp_log.h"
-#include "esp_hidd_api.h"
-#include "esp_bt_main.h"
-#include "esp_bt_device.h"
-#include "esp_bt.h"
-#include "esp_err.h"
-#include "nvs.h"
-#include "nvs_flash.h"
-#include "esp_gap_bt_api.h"
-#include <string.h>
-#include <inttypes.h>
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
+#include "header.h"
 
 #define REPORT_PROTOCOL_MOUSE_REPORT_SIZE      (4)
 #define REPORT_BUFFER_SIZE                     REPORT_PROTOCOL_MOUSE_REPORT_SIZE
