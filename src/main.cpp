6da9edcf10523803ee13079258982c522fecfb16
dmmosh <evilsharkeater@gmail.com>
2025-10-23 01:16:04 -0400

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include <Arduino.h>
-
-#define CONTROL 15
-
-void setup() {
-  pinMode(CONTROL, OUTPUT);
-  // put your setup code here, to run once:
+/*
+ * LAB Name: ESP32 DAC Direct Voltage Output Example
+ * Author: Khaled Magdy
+ * DeepBlueMbedded 2023
+ * For More Info Visit: www.DeepBlueMbedded.com
+*/
+ 
+#define DAC_CH1 25
+ 
+void setup() 
+{
+  // DO NOTHING
 }
-
-void loop() {
-  digitalWrite(CONTROL,HIGH);
-  vTaskDelay(1000/portTICK_PERIOD_MS);
-  digitalWrite(CONTROL,LOW);
-  vTaskDelay(1000/portTICK_PERIOD_MS);
+ 
+void loop() 
+{
+  // Send Output DC Voltage Raw Values To The DAC
+  // (0  64  128 192 255)
+  // Insert Delay After Each Sample Output
+  dacWrite(DAC_CH1, 0);
+  delay(100);
+  dacWrite(DAC_CH1, 64);
+  delay(100);
+  dacWrite(DAC_CH1, 128);
+  delay(100);
+  dacWrite(DAC_CH1, 192);
+  delay(100);
+  dacWrite(DAC_CH1, 255);
+  delay(100);
 }
