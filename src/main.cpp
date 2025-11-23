a023a57919b163bbf3f817ade533ea072509ab47
dmmosh <evilsharkeater@gmail.com>
2025-11-23 17:34:21 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,14 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <VL53L1X.h>
+#define LED 2
 
-VL53L1X sensor;
-#define SDA 1
-#define SCL 2
+void setup(){
+  pinMode(LED, OUTPUT);
 
-void sensor_init(VL53L1X::DistanceMode range_mode, bool high_speed) {
-  Wire.setPins(SDA,SCL);
-  Wire.begin();
-  sensor.setTimeout(500);
-  sensor.init();
-  // sensor.setDistanceMode(range_mode);  
-  // int budget = high_speed ? 33000 : 140000;
-  // sensor.setMeasurementTimingBudget(budget);
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.print("hello world");
-  //range_mode: VL53L1X::Short, VL53L1X::Medium, or VL53L1X::Long
-  sensor_init(VL53L1X::Long, true);   
-}
-
-void loop() {
-  //Serial.print("loop\n");
-  int dist = sensor.readRangeSingleMillimeters();
-  Serial.printf("cm: %i.%i", dist/10, dist%10);
-  Serial.print("\tambient: ");
-  Serial.print(sensor.ranging_data.ambient_count_rate_MCPS);
-  Serial.print("\tpeak signal: ");
-  Serial.print(sensor.ranging_data.peak_signal_count_rate_MCPS);
-  Serial.println();
+void loop(){
+  digitalWrite(LED,HIGH);
+  delay(500);
+  digitalWrite(LED,LOW);
   delay(500);
 }
