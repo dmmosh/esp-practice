58323d16a8b6da87359f55b41f900319339d56ee
dmmosh <evilsharkeater@gmail.com>
2025-11-23 17:38:57 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #define LED 2
+#define DPLUS 13
+#define DMINUS 12
 
 void setup(){
+  Serial.begin(115200);
   pinMode(LED, OUTPUT);
-
+  pinMode(DPLUS,INPUT_PULLUP);
+  pinMode(DMINUS,INPUT_PULLUP);
 }
 
 void loop(){
+  Serial.printf("%i, %i\n", digitalRead(DPLUS), digitalRead(DMINUS));
   digitalWrite(LED,HIGH);
   delay(500);
   digitalWrite(LED,LOW);
